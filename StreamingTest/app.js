ae7fb33f5995b63231d6879eb4dc7de45170762e
YDee <yoavd1232@yahoo.com>
2015-02-26 18:19:47 +0200

    Stream
    First try

---
+++ StreamingTest/app.js
@@ -0,0 +1,15 @@
+﻿var fs = require('fs');
+var port = process.env.port || 1337;
+var express = require('express');
+var app = express();
+
+var path = __dirname + "/public";
+app.use(express.static(path));
+app.listen(port);
+
+app.get('/index/:text', function (request, response) {
+    console.log(response.params.text);
+    response.end();
+});
+
+
